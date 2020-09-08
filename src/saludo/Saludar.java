28fd4fca34a90549aab7fe44b3ceaee11f6e4b33
Sofia Bianco <b.sofia2124@gmail.com>
2020-09-08 12:48:00 -0300

    Saludo

---
+++ src/saludo/Saludar.java
@@ -0,0 +1,12 @@
+package saludo;
+
+public class Saludar {
+ 
+	public static void main(String[] args) {
+		
+		String mensaje;
+		mensaje = "¡Hola mundo desde Java!";
+		
+		System.out.println(mensaje);
+	}
+}
